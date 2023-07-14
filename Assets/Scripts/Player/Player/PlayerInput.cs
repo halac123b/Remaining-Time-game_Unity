c59ac2596cd481956db092af382997e9d58025c7
Nguyen Phuc Binh Khang <91519610+khangnpb@users.noreply.github.com>
2023-07-14 11:13:34 +0700

    Sap xep file

---
+++ Assets/Scripts/Player/Player/PlayerInput.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+  private PlayerInputAction playerInputActions;
+  private int TypeMove;
+  [SerializeField] CapsuleCollider2D capsuleCollider2D;
+  private void Awake()
+  {
+    playerInputActions = new PlayerInputAction();
+    playerInputActions.Player.Enable();
+    playerInputActions.Player.Run.performed += Run;
+    playerInputActions.Player.Duck.performed += Duck;
+  }
+
+  public Vector2 GetMovementVectorNormalized()
+  {
+    Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+    inputVector = inputVector.normalized;
+    return inputVector;
+  }
+
+  public int GetTypeMove(){
+    return TypeMove;
+  }
+   private void Run(InputAction.CallbackContext context){
+        if(context.ReadValueAsButton()){
+            TypeMove = 1;
+        }else TypeMove = 0;
+    }
+    private void Duck(InputAction.CallbackContext context){
+        if(context.ReadValueAsButton()){
+            TypeMove = -1; 
+            capsuleCollider2D.offset = new Vector2(capsuleCollider2D.offset.x,capsuleCollider2D.offset.y-0.2f);
+            capsuleCollider2D.size = new Vector2(capsuleCollider2D.size.x,capsuleCollider2D.size.y - 0.4f);
+        }else {
+            TypeMove = 0;
+            capsuleCollider2D.offset = new Vector2(capsuleCollider2D.offset.x,capsuleCollider2D.offset.y+0.2f);
+            capsuleCollider2D.size = new Vector2(capsuleCollider2D.size.x,capsuleCollider2D.size.y + 0.4f);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerMovement : NetworkBehaviour
+{
+  private float moveSpeed = 5f;
+  private PlayerInput playerInput;
+
+  private Vector2 moveVector;
+  private Vector2 lastDirection;
+
+  private void Awake()
+  {
+    lastDirection = new Vector2(0, 1);
+    playerInput = FindObjectOfType<PlayerInput>();
+  }
+
+  private void Update()
+  {
+    if (!IsOwner)
+    {
+      return;
+    }
+    HandleMovement();
+    if(playerInput.GetTypeMove()== 1){
+      moveSpeed = 7f;
+    }else if(playerInput.GetTypeMove()== 0){
+      moveSpeed = 5f;
+    } 
+    
+    if(playerInput.GetTypeMove()== -1){
+      moveSpeed = 3f;
+     }
+  }
+
+  public Vector2 MoveVector()
+  {
+    return moveVector;
+  }
+
+  public Vector2 LastDirection()
+  {
+    return lastDirection;
+  }
+
+  private void HandleMovement()
+  {
+    Vector2 inputVector = playerInput.GetMovementVectorNormalized();
+    float moveDistance = moveSpeed * Time.deltaTime;
+
+
+    transform.position += new Vector3(inputVector.x,inputVector.y) * moveDistance;
+
+    lastDirection = moveVector;
+    moveVector = inputVector;
+  }
+  public int GetTypeMove(){
+        return playerInput.GetTypeMove();
+  }
+  public void SetSpeed(float speed){
+      moveSpeed = speed;
+  }
+}
+
+// Fix: Remove LastDirection
